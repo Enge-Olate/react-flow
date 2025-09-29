bd772c75d61bfca9c841006bb3da7ee6d646c5fd
Enge-Olate <marcioalexisolate@live.com>
2025-09-28 22:47:29 -0300

    Iniciando com arrays, inputs e usestate.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 const nome = 'Olate';
+const lista = ['banana', 'limão', 'abacate'];
+const [name, setNname] = useState('');
   return (
     <>
       <h1 className='nome'>{nome}</h1>
+      <ul>
+        {lista.map(item=>(
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+      <input type="text"
+        value={name}
+        onChange={name2 =>{setNname(name2.target.value)}}
+      />
+      <h2>{name}</h2>
     </>
   )
 }
